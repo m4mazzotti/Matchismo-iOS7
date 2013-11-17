1d4af614e8b4aa96e6dc7dc9305bce97d5187175
Marcelo Mazzotti <m4mazzotti@Marcelos-MacBookPro.local>
2013-11-17 21:37:27 -0200

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Marcelo Mazzotti on 17/11/13.
+//  Copyright (c) 2013 Marcelo Mazzotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
